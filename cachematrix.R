ec86600bc68bbe31763fa53edd406dba83d4382a
Jyothismaria <jyohtismariajoseph@gmail.com>
2023-06-05 09:46:48 -0400

    Adding the function to leverage caching to reduce the compute time taken to inverse a mtrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  function creates a special "matrix" object that can cache its inverse
+## list contain get()set() getinverse and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  isInvertible <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+    isInvertible <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+  setisInvertible <- function(is) isInvertible <<- is 
+  getisInvertible <- function() isInvertible 
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse,
+       getisInvertible = getisInvertible,
+       setisInvertible  = setisInvertible )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve if the data is cached if not it calculates the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  is <- x$getisInvertible()
+  if(!is.null(is)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  if (det(data) != 0){
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    x$setisInvertible(T)
+  }
+  else {
+    x$setinverse(NULL)
+    x$setisInvertible(F)
+  }
+  inverse
 }
